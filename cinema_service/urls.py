e0b93ac28a2af47fa4786edf74ebd58c95d4d4c7
vavarvariuk <vasya.varvariuk.27@gmail.com>
2025-02-15 13:49:03 +0200

    solution

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-path("api/cinema/", include("cinema.urls", namespace="cinema")),
+    path("api/cinema/", include("cinema.urls", namespace="cinema")),
 ]
 if settings.DEBUG:
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
